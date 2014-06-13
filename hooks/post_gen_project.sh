c71eaf4cc5a1107276e788a90a986291379268d8
Adam Cupiał <adam.cupial@codepill.com>
2014-06-13 14:29:10 +0200

    auto install off

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,12 +3,7 @@ echo
 echo "************* Initializing GIT **************"
 echo
 command -v git >/dev/null 2>&1 && git init || { echo >&2 "git init is failed. Probably git is not installed. Install git if it's not installed."; exit 1; }
-
-echo
-echo "************* Building project **************"
-echo
-make install
-
 echo
 echo "************* Build finished, start working! **************"
+echo "Begin by going to your project's dir and running make install"
 echo
