a77f0ad5219bd7ecc4d398593a02574463c8d1cd
Adam Cupiał <adam.cupial@codepill.com>
2014-06-16 08:50:59 +0200

    removed db questions from cookiecutter

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,5 +5,8 @@ echo
 command -v git >/dev/null 2>&1 && git init || { echo >&2 "git init is failed. Probably git is not installed. Install git if it's not installed."; exit 1; }
 echo
 echo "************* Build finished, start working! **************"
-echo "Begin by going to your project's dir and running 'make install'"
+echo "1. cd to your project dir"
+echo "2. cp local.cfg-dist to local.cfg (git-ignored by default)"
+echo "3. edit local.cfg-dist and set up database"
+echo "4. make install"
 echo
